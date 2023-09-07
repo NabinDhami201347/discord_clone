db31ccfdd2a1af9f6f4098b39de8f812f2a38f1b
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-07 08:21:00 +0545

    feat: create server modal

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -41,14 +41,14 @@ const NavigationSidebar = async () => {
 
       <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
         <ModeToggle />
-        <UserButton
+        {/* <UserButton
           afterSignOutUrl="/"
           appearance={{
             elements: {
               avatarBox: "h-[48px] w-[48px]",
             },
           }}
-        />
+        /> */}
       </div>
     </div>
   );
