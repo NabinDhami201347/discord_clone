082fa6b84d0c434e8870c29ae5626e55aa1b9324
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-07 07:47:06 +0545

    feat: sidebar

---
+++ components/action.tooltip.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+interface IToolTipProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+}
+const ActionTooltip = ({ label, align, side, children }: IToolTipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="font-semibold text-sm capitalize">{label.toLowerCase()}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default ActionTooltip;
