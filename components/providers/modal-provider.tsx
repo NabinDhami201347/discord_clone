18c21058c7e9c1bfa35f56c19b461170a9d0cdfa
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-14 07:44:40 +0545

    feat: create channel

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -6,6 +6,7 @@ import InviteModal from "@/components/modals/invite-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
+import { CreateChannelModal } from "../modals/create-channel-modal";
 
 export const ModalProvider = () => {
   const [mounted, setMounted] = useState(false);
@@ -22,6 +23,7 @@ export const ModalProvider = () => {
       <InviteModal />
       <EditServerModal />
       <MembersModal />
+      <CreateChannelModal />
     </>
   );
 };
