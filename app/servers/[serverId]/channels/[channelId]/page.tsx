6abab7937ca6ec441cdef6a115bf66bdd98be19c
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-15 06:49:16 +0545

    feat: mobile nav and chat header component

--- app/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,35 @@
-const ChannelId = () => {
-  return <div>ChannelId</div>;
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { FC } from "react";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+import { ChatHeader } from "@/components/chat/chat-header";
+interface ChannelIdProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelId: FC<ChannelIdProps> = async ({ params }) => {
+  const profile = await currentProfile();
+  if (!profile) return redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: { id: params.channelId },
+  });
+  const member = await db.member.findFirst({
+    where: { serverId: params.serverId, profileId: profile.id },
+  });
+
+  if (!channel || !member) return redirect("/");
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+    </div>
+  );
 };
 
 export default ChannelId;
