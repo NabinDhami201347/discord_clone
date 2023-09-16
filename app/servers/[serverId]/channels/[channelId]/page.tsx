eb01f38ce6754f37481409115dfffeba4cee0423
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-16 07:48:37 +0545

    feat: message input and route

--- app/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/servers/[serverId]/channels/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { FC } from "react";
 import { db } from "@/lib/db";
 import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 interface ChannelIdProps {
   params: {
     serverId: string;
@@ -28,6 +29,13 @@ const ChannelId: FC<ChannelIdProps> = async ({ params }) => {
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+      <div className="flex-1"></div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{ channelId: channel.id, serverId: channel.serverId }}
+      />
     </div>
   );
 };
