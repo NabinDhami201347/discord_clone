5fa600d9854272fe4196c7f72a9187fd50e26eec
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-09-17 07:02:29 +0545

    fix: web socket error

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -20,8 +20,6 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
       addTrailingSlash: false,
     });
     res.socket.server.io = io;
-
-    console.log("Request comming");
   }
 
   res.end();
